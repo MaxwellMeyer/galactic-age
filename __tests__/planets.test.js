1f8eff9851078ebc5a4ce1148f0622d1f92962f7
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-25 10:36:31 -0700

    good initial failed test for Planet constructor

---
+++ __tests__/planets.test.js
@@ -0,0 +1,9 @@
+import Planet from "../src/js/planets.js";
+
+describe("Planet", () => {
+  test("should instantiate a planet object with 2 keys", () => {
+    const planetAge = new Planet(5);
+    expect(planetAge.age).toEqual(5);
+    expect(planetAge.expectancy).toEqual(72);
+  });
+});
