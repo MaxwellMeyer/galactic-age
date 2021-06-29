eb8b8f079d8dbbe5f601577f9ad665d792d1ded4
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-28 21:40:13 -0700

    fix main.js format

--- src/main.js
+++ src/main.js
@@ -11,29 +11,29 @@ $(document).ready(function () {
     let lifeEx = 72;
     let galacticAge;
     switch (planet) {
-      case "mercury":
-        galacticAge = new Planet(age, lifeEx);
-        $("#spaceAge").text(galacticAge.mercuryAge());
-        $("#thePlanet").text(galacticAge.mercuryExp());
-        break;
-      case "mars":
-        galacticAge = new Planet(age, lifeEx);
-        $("#spaceAge").text(galacticAge.marsAge());
-        $("#thePlanet").text(galacticAge.mercuryExp());
-        break;
-      case "venus":
-        galacticAge = new Planet(age, lifeEx);
-        $("#spaceAge").text(galacticAge.venusAge());
-        $("#thePlanet").text(galacticAge.venusExp());
-        break;
-      case "jupiter":
-        galacticAge = new Planet(age, lifeEx);
-        $("#spaceAge").text(galacticAge.jupiterAge());
-        $("#thePlanet").text(galacticAge.jupiterExp());
-        break;
-      default:
-        $(".results").text("Please enter your age/planet!!");
-        break;
+    case "mercury":
+      galacticAge = new Planet(age, lifeEx);
+      $("#spaceAge").text(galacticAge.mercuryAge());
+      $("#thePlanet").text(galacticAge.mercuryExp());
+      break;
+    case "mars":
+      galacticAge = new Planet(age, lifeEx);
+      $("#spaceAge").text(galacticAge.marsAge());
+      $("#thePlanet").text(galacticAge.mercuryExp());
+      break;
+    case "venus":
+      galacticAge = new Planet(age, lifeEx);
+      $("#spaceAge").text(galacticAge.venusAge());
+      $("#thePlanet").text(galacticAge.venusExp());
+      break;
+    case "jupiter":
+      galacticAge = new Planet(age, lifeEx);
+      $("#spaceAge").text(galacticAge.jupiterAge());
+      $("#thePlanet").text(galacticAge.jupiterExp());
+      break;
+    default:
+      $(".results").text("Please enter your age/planet!!");
+      break;
     }
     $(".results").show();
   });
